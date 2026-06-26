af6cac26d19127703037c2c2ade140b926f9efc6
ankita Bhattarai <bhattaraiankita26@gmail.com>
2026-06-26 18:58:10 +0545

    Adding Teacher Controller

---
+++ Backend/controllers/teacherController.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const {getProfile, markAttendance, getAttendanceByCourse, enterResult, getStudentsByCourse, getPendingLeaves, reviewLeave}= require('../controllers/teacherController')
+const {protect} = require('../middleware/authMiddleware')
+const { authorize} = require('../middleware/roleMiddleware')
+
+router.use(protect, authorize('teacher','admin'))
+
+router.get('/profile', getProfile)
+router.get('/attendance', markAttendance)
+router.get('/attendance/:courseId', getAttendanceByCourse)
+router.post('/results', enterResult)
+router.get('/students/:courseId', getStudentsByCourse )
+router.get('/leaves', getPendingLeaves)
+router.put('/leaves/:id', reviewLeave)
+
+module.exports = router
